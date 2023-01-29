fb50eefcb35905f03db3ee413c498eb7bc2482db
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-29 23:03:48 +0900

    사용자 정의 리포지토리

---
+++ src/main/java/study/querydsl/reqository/MemberRepositoryCustom.java
@@ -0,0 +1,10 @@
+package study.querydsl.reqository;
+
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+    List<MemberTeamDto> search(MemberSearchCondition condition);
+}

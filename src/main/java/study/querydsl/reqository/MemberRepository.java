b0da0f7cb52225e78db6fac02419a0fde777a6e9
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-29 23:41:06 +0900

    인터페이스 지원 - QuerydslPredicateExecutor

--- src/main/java/study/querydsl/reqository/MemberRepository.java
+++ src/main/java/study/querydsl/reqository/MemberRepository.java
@@ -1,11 +1,12 @@
 package study.querydsl.reqository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> ,MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long> ,MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
     // select m from Member m where m.username = ?
     List<Member> findByUsername(String username);
 }
